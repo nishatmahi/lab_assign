2eecc96c060c27f29242d794e43c8104776d1572
nishatmahi <mahitasnim03@gmail.com>
2023-03-21 18:06:24 +0600

    first commit

---
+++ app/src/main/java/com/hma/for_assign/lab.java
@@ -0,0 +1,17 @@
+package com.hma.for_assign;
+
+public class lab {
+    int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public lab(int id) {
+        this.id = id;
+    }
+}
